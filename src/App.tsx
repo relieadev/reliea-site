f87a52f031ef8e0d5fdad2a33c26fb635f31a802
reliea <relieadev@proton.me>
2026-09-12 15:45:00 +0900

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const websiteName = "Reliea's Website";
+
+  return(
+    <main>
+      <h1>{websiteName}</h1>
+      <p>Reliea's introduction site</p>
+    </main>
+    )
+}
+
+export default App
