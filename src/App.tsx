9e604353b7bc06ddee172916f6405dc2462035b1
reliea <relieadev@proton.me>
2026-09-12 17:07:55 +0900

    Create Header and Footer conponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-function App() {
+function Header() {
   const websiteName = "Reliea's Website";
 
   return(
-    <main>
+      <header>
       <h1>{websiteName}</h1>
       <p>Reliea's introduction site</p>
+
+      <nav className="hpNav">
+        <a href="/introduction">Introduction</a>
+        <a href="/work">Work</a>
+        <a href="study-logs">StudyLogs</a>
+        <a href="blog">Blog</a>
+        <a href="Playground">Playground</a>
+      </nav>
+      </header>
+  )
+}
+
+function Footer() {
+
+  return(
+    <footer>
+      <p>©︎ 2026 Reliea Introduced-page</p>
+    </footer>
+  )
+}
+
+function App() {
+
+  return(
+    <main>
+      <Header />
+
+      <Footer />
     </main>
     )
 }
