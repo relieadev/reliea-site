4f5f7b790bf820fe8efd0459431b1bfbd5a8d92f
reliea <relieadev@proton.me>
2026-09-12 19:18:18 +0900

    fix css and add a hunbrger-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,61 @@
 import { useState } from "react";
-import "./App.css"
+import "./App.css";
 
 function Header() {
-  const websiteName = "Reliea's Website";
-  const [menuOpen, setMenuOpen] = useState(false);
+    const websiteName = "Reliea's Website";
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  return(
-      <header className="hp-header">
-        <a className="brand" href="/">
-          {websiteName}
-        </a>
-        
-        <p>Reliea's introduction site</p>
+    return (
+        <header className="hp-header">
+            <div className="hp-header-brand">
+                <a className="brand" href="/">
+                    {websiteName}
+                </a>
 
-        <button
-          className="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-expanded={menuOpen}
-          aria-label="Menu"
-        >
-          
-          ☰
-        
-        </button>
+                <p>Reliea's introduction site</p>
+            </div>
 
-        <nav className={'hp-nav ${menuOpen ? "is-Open" : ""}'}>
-          <a href="/introduction">Introduction</a>
-          <a href="/work">Work</a>
-          <a href="study-logs">StudyLogs</a>
-          <a href="blog">Blog</a>
-          <a href="playground">Playground</a>
-        </nav>
-      </header>
-  )
+            <button
+                className="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+                aria-expanded={menuOpen}
+                aria-label="Menu"
+            >
+                ☰
+            </button>
+
+            <nav className={`hp-nav ${menuOpen ? "is-open" : ""}`}>
+                <a href="/introduction">Introduction</a>
+                <a href="/work">Work</a>
+                <a href="/study-logs">StudyLogs</a>
+                <a href="/blog">Blog</a>
+                <a href="/playground">Playground</a>
+            </nav>
+        </header>
+    );
 }
 
 function Footer() {
-
-  return(
-    <footer className="hp-footer">
-      <p>©︎ 2026 Reliea Introduced-page</p>
-    </footer>
-  )
+    return (
+        <footer className="hp-footer">
+            <p>© 2026 Reliea Introduced-page</p>
+        </footer>
+    );
 }
 
 function App() {
+    return (
+        <main className="hp-main">
+            <Header />
 
-  return(
-    <main className="hp-main">
-      <Header />
+            <section className="hp-hero">
+                <h1>Welcome to Reliea's Website</h1>
+                <p>Build, Learn, and Explore.</p>
+            </section>
 
-      <section className="hp-content">
-        <h1>Welcome to Reliea's Website</h1>
-        <p>Build, Learn, and Explore.</p>
-      </section>
-
-      <Footer />
-    </main>
-    )
+            <Footer />
+        </main>
+    );
 }
 
-export default App
+export default App;
